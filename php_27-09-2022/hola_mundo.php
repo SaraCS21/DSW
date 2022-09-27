32fd54c0c0363a83ed400018fcd7e1da61d8f781
Sara <saracs15.scss@gmail.com>
2022-09-27 20:56:50 +0100

    Iniciando php

---
+++ php_27-09-2022/hola_mundo.php
@@ -0,0 +1,21 @@
+<?php
+    echo "<h1 style='text-align: center;'>Hola Mundo</h1>";
+    print "<p style='text-align: center;'> Hola Mundo </p>";
+
+    // Comentarios de una línea
+    # Comentarios de una línea
+
+    /* Comentarios
+    de varias
+    líneas */
+
+    {
+        // Bloques de código
+    }
+
+    $variable = "Hola"; 
+    $CONSTANTES;  // Se puede cambiar el valor
+
+    print '$variable';   // Con la comillas simples no sirven las variables, detecta todo como texto
+    print "$variable";   // Muestra el valor de la variable correctamente
+?>
