7e9d7bc59fc7ad2a7521d40cb5f00304e96f02e0
Sara <saracs15.scss@gmail.com>
2022-09-29 19:25:41 +0100

    Corregir act12

--- php_28-09-2022/act12.php
+++ php_28-09-2022/act12.php
@@ -4,18 +4,20 @@
     $array_enteros = explode(",", $cadena_enteros);
     $cadena_nueva = "";
 
-    for ($i=0; $i<=count($array_enteros)-1; $i++){
+    for ($i=0; $i<=count($array_enteros); $i++){
 
         $salto = $array_enteros[$i];
         $cadena_nueva .= $array_enteros[$i] . "," . str_repeat("_,", $salto);
 
-        $i += $salto;
-
-        if ($salto == 0){
+        if ($array_enteros[$i] == 0){
             break;
         };
+
+        $i += $salto;
     };
 
     echo $cadena_nueva;
 
 ?>
+
+// HACER CON WHILE
