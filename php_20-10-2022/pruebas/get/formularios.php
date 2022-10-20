ffa8399d57d2079f785aafa7b2eeb989527416f6
Sara <saracs15.scss@gmail.com>
2022-10-20 16:32:16 +0100

    pruebas get y post

---
+++ php_20-10-2022/pruebas/get/formularios.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Forms GET</title>
+</head>
+<body>
+
+    <form method="get" action="page1.php">
+        <input type="text" name="name">
+        <label for="name">Write your name</label>
+        <br><br>
+        <input type="text" name="surname">
+        <label for="surname">Write your surname</label>
+        <br><br>
+        <input type="text" name="address">
+        <label for="address">Write you address</label>
+        <br><br>
+        <input type="text" name="phone" maxlength="9" pattern="[0-9]{9}">
+        <label for="phone">Write your phone</label>
+        <br><br>
+        <input type="submit" value="SEND">
+        <br><br>
+    </form>
+
+</body>
+</html>
---
+++ php_20-10-2022/pruebas/post/page1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Forms POST</title>
+</head>
+<body>
+    <?php
+        echo "<pre>";
+        print_r($_POST);
+        echo "</pre>";
+    ?>
+</body>
+</html>
