6760a8c822b2f03be0dae96158026df15d3addd8
SaraCS21 <saracs15.scssn@gmail.com>
2022-11-18 13:06:10 +0000

    php

--- examen/examen/a.php
+++ examen/examen/a.php
@@ -12,4 +12,14 @@
     // uft8_encode() -> si da fallos de caracteres especiales
     $final = strftime("__", $date);
 
+    // Sanitizar -> trim() | strip_tags() | str_replace()
+    // header("Location:url");
+    // $url -> $_SERVER[“PHP_SELF”]
+
+    // <form method="post" action=$url enctype="multipart/form-data">
+    // <input type="hidden" name="MAX_FILE_SIZE" value="30000"/>
+    is_uploaded_file($_FILES["nombre_fichero"]["tmp_name"]);
+    $_FILES["nombre_fichero"]["type"];
+    move_uploaded_file($_FILES["nombre_fichero"]["tmp_name"], $carpeta);
+
 ?>
