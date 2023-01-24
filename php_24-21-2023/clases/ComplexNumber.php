20454c0994646e55337b8e18fb8ddb95ba20ec34
Sara <saracs15.scss@gmail.com>
2023-01-24 20:55:40 +0000

    clases con php

--- php_24-21-2023/clases/ComplexNumber.php
+++ php_24-21-2023/clases/ComplexNumber.php
@@ -9,6 +9,14 @@
             $this->imaginario = $imaginario;
         }
 
+        public function getReal(){
+            return $this->real;
+        }
+
+        public function getImaginario(){
+            return $this->imaginario;
+        }
+
         public function getComplexNumber(){
             if ($this->imaginario < 0){
                 echo $this->real . $this->imaginario . "i";
@@ -17,9 +25,20 @@
                 echo $this->real . "+" . $this->imaginario . "i";
             }
         }
+
+        public function sumComplexNumber($number){
+            $real = $this->real + $number->getReal();
+            $imaginario = $this->imaginario + $number->getImaginario();
+
+            return [$real, $imaginario];
+        }
     }
 
     $number1 = new ComplexNumber(2, -3);
-    $number1->getComplexNumber();
+    $number2 = new ComplexNumber(2, -3);
+
+    $result = $number2->sumComplexNumber($number1);
+    $number3 = new ComplexNumber($result[0], $result[1]);
+    $number3->getComplexNumber();
 
 ?>
